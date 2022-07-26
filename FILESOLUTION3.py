a83f6308083a6af0c0aa64fc8217a2c17615f17a
Chiso <chiboguchisomu@gmail.com>
2022-07-26 11:47:38 +0100

    Added function to save in folder and convert to zip

--- FILESOLUTION3.py
+++ FILESOLUTION3.py
@@ -4,6 +4,7 @@ import json
 import math
 from zipfile import ZipFile
 
+
 if sys.version_info[0] < 3:
     print('This script requires Python 3 or higher')
     exit()
@@ -11,58 +12,72 @@ if sys.version_info[0] < 3:
 print('Welcome to the JSON Splitter')
 print('First, enter the name of the file you want to split')
 
-try:
     # request file name
-    file_name = input('filename: ')
-    f = open(file_name)
-    file_size = os.path.getsize(file_name)
-    data = json.load(f)
+file_name = input('filename: ')
+f = open(file_name)
+file_size = os.path.getsize(file_name)
+data = json.load(f)
 
-    if isinstance(data, list):
-        data_len = len(data)
-        print('Valid JSON file found')
-    else:
-        print("JSON is not an Array of Objects")
-        exit()
 
-except:
-    print('Error loading JSON file ... exiting')
-    exit()
+data_len = len(data)
+print('Valid JSON file found')
+
+
 
 # get numeric input
-try:
-    mb_per_file = abs(float(input('Enter maximum file size (MB): ')))
-except:
-    print('Error entering maximum file size ... exiting')
-    exit()
+
+mb_per_file = abs(float(input('Enter maximum file size (MB): ')))
+chunk_size = abs(int(input('Enter chunk size (number of objects): ')))
+if chunk_size > data_len:
+    chunk_size = data_len
+if chunk_size < 1:
+    chunk_size = 1
+if mb_per_file < 1:
+    mb_per_file = 1
+
 
 # check that file is larger than max size
-if file_size < mb_per_file * 1000000:
-    print('File smaller than split size, exiting')
+    if file_size > mb_per_file * 1024 * 1024:
+        print('File is too large to split')
+        exit()
+
+
+# calculate number of chunks
+num_chunks = math.ceil(data_len / chunk_size)
+print(f'Splitting {file_name} into {num_chunks} chunks')
+print(f'Each chunk will be {mb_per_file}MB')
+
+# create directory for chunks
+if not os.path.exists('chunks'):
+        os.makedirs('chunks')
+else:
+    print('chunks directory already exists')
+
+
+# split file
+for i in range(num_chunks):
+    with open(f'chunks/{file_name}_{i}.json', 'w') as outfile:
+        json.dump(data[i*chunk_size:(i+1)*chunk_size], outfile)
+
+# zip chunks
+with ZipFile('chunks.zip', 'w') as zip:
+    for file in os.listdir('chunks'):
+        zip.write('chunks/' + file)
+print('Chunks zipped into chunks.zip')
+print('Done')
+exit()
+
+# if file is not large enough to split
+if file_size < mb_per_file * 1024 * 1024:
+    print('File is too small to split')
     exit()
 
-# determine number of files necessary
-num_files = math.ceil(file_size/(mb_per_file*1000000))
-print('File will be split into', num_files, 'equal parts')
+# if file is not a valid JSON file
+if not data:
+    print('Invalid JSON file')
+    exit()
 
-# initialize 2D array
-split_data = [[] for i in range(0, num_files)]
 
-# determine indices of cutoffs in array
-starts = [math.floor(i * data_len/num_files) for i in range(0, num_files)]
-starts.append(data_len)
 
-# loop through 2D array
-for i in range(0, num_files):
-    # loop through each range in array
-    for n in range(starts[i], starts[i+1]):
-        split_data[i].append(data[n])
 
-    # create file when section is complete
-    name = os.path.basename(file_name).split('.')[0] + '_' + str(i+1) + '.json'
-    with open(name, 'w') as outfile:
-        json.dump(split_data[i], outfile)
 
-    print('Part', str(i+1), '... completed')
-
-print('Success! Script Completed')
